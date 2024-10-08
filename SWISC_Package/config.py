58912656235b244c62e4d0f57dfe363895464d52
bharvey-neuro <72761678+bharvey-neuro@users.noreply.github.com>
2024-10-08 13:01:30 -0700

    Update config.py

--- SWISC_Package/config.py
+++ SWISC_Package/config.py
@@ -2,7 +2,7 @@
 recording_length_hours=12
 sampling_freq=2000
 sampling_freq_dec=int(sampling_freq/10)
-epoch_length=2
+epoch_length=4
 recording_length_seconds=int(recording_length_hours*3600)
 target_epoch_count=int(recording_length_seconds/epoch_length)
 epoch_samples_dec=int(epoch_length*sampling_freq/10)
